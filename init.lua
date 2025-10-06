cb8805b9fc3a08cab7ac9a169c0372adf354d968
rafftoubol <raphael.toubol@gmail.com>
2025-10-06 11:51:09 +0200

    latest config

--- init.lua
+++ init.lua
@@ -246,6 +246,13 @@ rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+
+  {
+    'supermaven-inc/supermaven-nvim',
+    config = function()
+      require('supermaven-nvim').setup {}
+    end,
+  },
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
